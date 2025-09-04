64616f819552be29e8289d6307573b8d7419123d
Sakshant Salke <salkesakshant@gmail.com>
2025-09-05 00:45:40 +0530

    Time: 1 ms (100.00%) | Memory: 47.5 MB (35.77%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.java
@@ -1,20 +1,36 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
         //Two Pointers Approach
+        // int left = 0;
+        // int right = nums.length-1;
+        // int index = nums.length-1;
+        // int[] result = new int[nums.length];
+        // while(left <= right){
+        //     if(Math.abs(nums[left]) > Math.abs(nums[right])){
+        //         result[index] = nums[left]*nums[left];
+        //         left++;
+        //     }
+        //     else{
+        //         result[index] = nums[right]*nums[right];
+        //         right--;
+        //     }
+        //     index--;
+        // }
+        // return result;
+
         int left = 0;
         int right = nums.length-1;
-        int index = nums.length-1;
         int[] result = new int[nums.length];
-        while(left <= right){
+
+        for(int i=nums.length-1; i>=0; i--){//Starting from last index or Reversing the loop for    non-decreasing order of array
             if(Math.abs(nums[left]) > Math.abs(nums[right])){
-                result[index] = nums[left]*nums[left];
+                result[i] = nums[left]*nums[left];
                 left++;
             }
             else{
-                result[index] = nums[right]*nums[right];
+                result[i] = nums[right]*nums[right];
                 right--;
             }
-            index--;
         }
         return result;
     }
