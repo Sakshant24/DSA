add2d0ecc477af326fa56c9ea8fb71c8fc84fd1d
Sakshant Salke <salkesakshant@gmail.com>
2025-09-08 22:16:36 +0530

    Time: 1 ms (52.16%) | Memory: 41.2 MB (24.09%) - LeetSync

--- 263-ugly-number/ugly-number.java
+++ 263-ugly-number/ugly-number.java
@@ -1,15 +1,23 @@
 class Solution {
     public boolean isUgly(int n) {
+        // if(n<=0){
+        //     return false;
+        // }
+        // int[] primefactors ={2, 3, 5};
+        // for(int factor : primefactors){
+        //     while(n % factor == 0){
+        //         n = n / factor;
+        //     }
+        // }
+        // return n == 1;
+
         if(n<=0){
             return false;
         }
-        int[] primefactors ={2, 3, 5};
-        for(int factor : primefactors){
-            while(n % factor == 0){
-                n = n / factor;
-            }
-        }
-        return n == 1;
+        while(n % 2 == 0)   n = n/2;
+        while(n % 3 == 0)   n = n/3;
+        while(n % 5 == 0)   n = n/5;
 
+        return n == 1;
     }
 }
